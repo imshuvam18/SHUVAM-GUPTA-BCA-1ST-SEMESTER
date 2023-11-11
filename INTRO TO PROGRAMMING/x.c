5b22be6147eab31e06ec20eea06e90d873cad07b
HackieShubh <144257371+imshuvam18@users.noreply.github.com>
2023-11-11 21:23:05 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/x.c
@@ -0,0 +1,16 @@
+//WAP in c to print thE ASTERISK PATTERN
+
+#include<stdio.h>
+int main(){
+    int rows;
+    printf("ENTER THE ROWS : ");
+    scanf("%d", &rows);
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=i;j++)
+        {
+            printf(" *");
+
+        }
+        printf("\n");
+    }
+}
