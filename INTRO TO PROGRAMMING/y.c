2d96ab5568cf2a37241c9614dad2f08a9af44df7
HackieShubh <144257371+imshuvam18@users.noreply.github.com>
2023-11-12 21:36:08 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/y.c
@@ -0,0 +1,15 @@
+// NESTED LOOP EXAMPLE
+#include<stdio.h>
+int main(){
+    int start,stop;
+    printf("ENTER THE START VALUE : ");
+    scanf("%d", start);
+     printf("ENTER THE STOP VALUE : ");
+    scanf("%d", stop);
+    for(int i=start;i<=stop;i++){
+        for(int j=1;j<=10;j++ ){
+            printf("%d*%d=%d",i,j,i*j);
+        }
+    }
+
+}
